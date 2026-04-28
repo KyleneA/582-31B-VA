d9d0ac8d318328a8cc596ccc140716f6b6ad5726
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-04-28 10:17:24 -0400

    Changing new_price method to set_price
    to follow writing conventions

--- class03/exercises/exerciseK01.py
+++ class03/exercises/exerciseK01.py
@@ -13,10 +13,10 @@ class Fruit:
     def get_category(self):
         return self.category
     
-    def new_price(self, new_price):
+    def set_price(self, new_price):
         self.price = new_price
         return self.price
 
 fruit1 = Fruit("banana", 2.99, "berry")
 print(fruit1.get_name(), fruit1.get_category(), fruit1.get_price())
-print(fruit1.new_price(1.99))
+print(fruit1.set_price(0.99))
