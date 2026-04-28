7534a932bd3bd68f03ff4de2f8436e6d70986316
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-04-28 11:28:50 -0400

    Adding my solution for exercise02

--- class03/exercises/exerciseK02.py
+++ class03/exercises/exerciseK02.py
@@ -5,6 +5,7 @@ class User:
         else:
             self.username = username
             self.password = password
+    
     def set_password(self, new_password):
         if len(new_password) < 4:
             print("Password was not changed. Password needs to be at least 4 characters long")
