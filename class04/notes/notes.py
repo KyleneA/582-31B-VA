520b92dcf46285717218c9811d2717745338c4f4
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-04-29 09:32:52 -0400

    initializing class04 folder

---
+++ class04/notes/notes.py
@@ -0,0 +1,9 @@
+# instance attribute -> 
+    ## created with self, 
+    ## usually defined in def __init, 
+    ## different per object
+
+# class attribute -> 
+    ## defined in class body, 
+    ## shared across all instances
+
