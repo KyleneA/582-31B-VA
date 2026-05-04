5a425f946bd86d674422ae59dbdb01b70dd4ac07
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-04 17:12:21 -0400

    Adding solution for exercise 4 and adding print lines to distinguish the different exercises

--- class04/exercisesKylene/hw_exercise02.py
+++ class04/exercisesKylene/hw_exercise02.py
@@ -28,6 +28,7 @@ class Student:
     def display_info(self):
         print(f"{self.name} studies {self.program} at {self.school_name}. Grade: {self.grade}")
 
+print(f"--- students ---")
 stu1 = Student("bob", "maths", 88).display_info()
 stu2 = Student("rob", "maths", 68).display_info()
 stu3 = Student("ron", "maths", 78).display_info()
@@ -49,7 +50,53 @@ prod1 = Product("calculator", 20)
 prod2 = Product("mouse", 10)
 prod3 = Product("keyboard", 100)
 
+print(f"--- original ---")
 print(prod1.price_with_tax(), prod2.price_with_tax(), prod3.price_with_tax())
 
 Product.tax_rate = 0.2
+print(f"--- 0.2 tax_rate ---")
 print(prod1.price_with_tax(), prod2.price_with_tax(), prod3.price_with_tax())
+
+# Exercise 4
+class Employee:
+    company_name = "TechNova"
+    bonus_rate = 0.10
+    employee_count = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+        Employee.employee_count += 1
+    
+    def calculate_bonus(self):
+        return self.salary * self.bonus_rate
+
+    def display_employee(self):
+        bonus = self.calculate_bonus()
+        print(f"{self.name} works at {self.company_name}. Salary: {self.salary}. Bonus: {bonus}")
+
+em1 = Employee("Bob", 40000)
+em2 = Employee("Rob", 40000)
+em3 = Employee("Ron", 40000)
+
+print(f"--- original ---")
+em1.display_employee()
+em2.display_employee()
+em3.display_employee()
+
+em3.bonus_rate = 0.5
+
+print(f"--- em3 bonus ---")
+em1.display_employee()
+em2.display_employee()
+em3.display_employee()
+
+Employee.bonus_rate = 0.05
+
+print(f"--- Employee bonus ---")
+em1.display_employee()
+em2.display_employee()
+em3.display_employee()
+
+# em3 has a shadowed bonus_rate because I changed it to 0.5 at the em3 bonus section
