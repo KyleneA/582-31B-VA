b83560028f017c1022087739059018ec8d177b71
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 10:17:51 -0400

    Adding instance methods to book class in book.py

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -3,6 +3,18 @@ class Book:
         self.title = title
         self.author = author
         self.available = available
-
-
-
+    
+    def borrow(self):
+        if (self.available):
+            self.available = False
+        else:
+            print(f"{self.title} by {self.author} is not available to borrow")
+    
+    def return_book(self):
+        if (self.available == False):
+            self.available = True
+        else:
+            print(f"This copy of {self.title} by {self.author} does not belong to us")
+    
+    def display_info(self):
+        print(f"{self.title} by {self.author}. Available: {self.available}")
