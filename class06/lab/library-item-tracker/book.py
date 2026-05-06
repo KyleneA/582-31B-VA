12f1d3480fc0c223aeddf9113f534d035cd630ff
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 10:38:53 -0400

    Adding book class library name class attribute and class method to change it in book.py and test code to see if the class method works

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -1,4 +1,10 @@
 class Book:
+    library_name = "Central Library"
+
+    @classmethod
+    def change_library_name(cls, new_name):
+        cls.library_name = new_name
+    
     def __init__(self, title, author, available):
         self.title = title
         self.author = author
--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -5,3 +5,9 @@ book2 = Book("Cat's Cradle", "Kurt Vonnegut", True)
 
 print(f"book 1: {book1.title} by {book1.author}. Available: {book1.available}")
 print(f"book 2: {book2.title} by {book2.author}. Available: {book2.available}")
+
+print()
+
+print(Book.library_name)
+Book.change_library_name("Montreal Central Library")
+print(Book.library_name)
