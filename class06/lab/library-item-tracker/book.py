4bdf68fe2bab88d93292a640898612d13cd297d7
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 10:00:22 -0400

    Moving book class to book.py file and adding import to main.py

---
+++ class06/lab/library-item-tracker/book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, available):
+        self.title = title
+        self.author = author
+        self.available = available
+
+
+
--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -1,12 +1,7 @@
-class Book:
-    def __init__(self, title, author, available):
-        self.title = title
-        self.author = author
-        self.available = available
+from book import Book
 
 book1 = Book("Kitchen", "Banana Yoshimoto", True)
 book2 = Book("Cat's Cradle", "Kurt Vonnegut", True)
 
 print(f"book 1: {book1.title} by {book1.author}. Available: {book1.available}")
 print(f"book 2: {book2.title} by {book2.author}. Available: {book2.available}")
-
