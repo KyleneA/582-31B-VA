df4db533a10c0e1d766696864b568d5c2df521b8
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 10:45:53 -0400

    Adding is valid title (static method) to book class and test code

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -4,7 +4,13 @@ class Book:
     @classmethod
     def change_library_name(cls, new_name):
         cls.library_name = new_name
-    
+
+    @staticmethod
+    def is_valid_title(title):
+        if (len(title) > 0):
+            return True
+        return False
+
     def __init__(self, title, author, available):
         self.title = title
         self.author = author
--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -7,7 +7,12 @@ print(f"book 1: {book1.title} by {book1.author}. Available: {book1.available}")
 print(f"book 2: {book2.title} by {book2.author}. Available: {book2.available}")
 
 print()
-
+# testing class method
 print(Book.library_name)
 Book.change_library_name("Montreal Central Library")
 print(Book.library_name)
+
+# testing static method
+title1 = Book.is_valid_title("")
+title2 = Book.is_valid_title("O")
+print(f"title1 is valid: {title1}; title2 is valid: {title2}")
