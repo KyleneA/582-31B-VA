215784a3580c4a5c0e9c08381312c871d2e3defe
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 09:42:36 -0400

    Adding book class and 2 book objects to main.py

---
+++ class06/lab/library-item-tracker/main.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author, available):
+        self.title = title
+        self.author = author
+        self.available = available
+
+book1 = Book("Kitchen", "Banana Yoshimoto", 1988, True)
+book2 = Book("Cat's Cradle", "Kurt Vonnegut", 1963, True)
+
