8b6668e84feb9dc2749393b7ef7cb1bc1ef730c1
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 09:50:37 -0400

    Updating book instances and printing attribute values in main.py

--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -4,6 +4,9 @@ class Book:
         self.author = author
         self.available = available
 
-book1 = Book("Kitchen", "Banana Yoshimoto", 1988, True)
-book2 = Book("Cat's Cradle", "Kurt Vonnegut", 1963, True)
+book1 = Book("Kitchen", "Banana Yoshimoto", True)
+book2 = Book("Cat's Cradle", "Kurt Vonnegut", True)
+
+print(f"book 1: {book1.title} by {book1.author}. Available: {book1.available}")
+print(f"book 2: {book2.title} by {book2.author}. Available: {book2.available}")
 
