f577470fac9a54efc625bd8fbf8fbd26b22976a0
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-06 10:19:38 -0400

    Adding test code to test new methods in main.py

--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -1,7 +1,19 @@
 from book import Book
 
 book1 = Book("Kitchen", "Banana Yoshimoto", True)
-book2 = Book("Cat's Cradle", "Kurt Vonnegut", True)
+book2 = Book("Cat's Cradle", "Kurt Vonnegut", False)
 
 print(f"book 1: {book1.title} by {book1.author}. Available: {book1.available}")
 print(f"book 2: {book2.title} by {book2.author}. Available: {book2.available}")
+
+print()
+
+# should print error msg
+book1.return_book()
+book2.borrow()
+
+# should not return error
+book1.borrow()
+book1.display_info()
+book2.return_book()
+book2.display_info()
