9ec22d8ae75ba7d879ffc14da2c0ae80316003b0
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-12 10:18:05 -0400

    adding my notes from class and initializing exercises files

---
+++ class07/notes/notes.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+	
+class Shape(ABC): # ABC means that this class is abstract
+    @abstractmethod
+    def area (shelf):
+        pass 
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+rectangle = Rectangle(2, 4)
+circle = Circle(3)
+
+print(f"rectangle area is: {rectangle.area()}; circle area is: {circle.area()}")
+
+print()
+
+def print_area(Shape):
+    print(Shape.area())
+
+print_area(rectangle)
+print_area(circle)
