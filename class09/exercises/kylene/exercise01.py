82c58eae1aeee7886fa3a4d55c0c5261aaf82e49
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-19 11:54:01 -0400

    Solution for exercise 04

--- class09/exercises/kylene/exercise01.py
+++ class09/exercises/kylene/exercise01.py
@@ -60,3 +60,17 @@ class Circle:
 circle = Circle(2)
 print(circle.area)
 print()
+
+# Exercise 04
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.__last_name = last_name
+    
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.__last_name}"
+
+person = Person("Bob", "builder")
+print(person.full_name)
+print()
