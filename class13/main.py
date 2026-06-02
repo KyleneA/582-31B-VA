71fdd1466d68c49d54eea72c611f5196d8f4f771
kamyar <kamyarkarimi98@gmail.com>
2026-06-02 09:51:48 -0400

    inheritance int.

--- class13/main.py
+++ class13/main.py
@@ -65,3 +65,73 @@ print(status.value)
 
 
 # all of the concepts above help us design one class properly. 
+
+
+# why inheritance exists?
+
+# Inheritance lets one class reuse and extend another class.
+
+# A parent class : A more general class
+
+# A child class : a more specific class that inherits from the parent
+
+# Suppose you are building different kinds of users in a system.
+
+# You might have: User - StudentUser - AdminUser
+# They may all share: 
+#   username
+#   email
+#   password
+#   name
+#   address
+#   phone_number
+
+# they may differ in:
+#   students have a program
+#   admins have departments
+
+# without inheritance, we have to repeat the code across several classes.
+# with inheritance, the child can reuse common parent behaviour.
+
+# Inheritance = shared structure + specialization
+
+class User:
+    def __init__(self, username):
+        print("hello from parent constructor")
+        self.username = username
+
+    def introduce(self):
+        print(f"Hello, my name is {self.username}")
+    
+
+class StudentUser(User): #extending the User class
+    def __init__(self, username, program):
+        super().__init__(username) # super means go to the parent class
+                                   # and in here we refer to the parent class' constructor
+        print("hello from child constructor")
+        self.program = program
+
+class AdminUser(User):
+    def __init__(self, username, dpt):
+        super().__init__(username)
+
+        self.dpt = dpt
+
+student1 = StudentUser("Jane", "Web Dev")
+student1.introduce() # StudentUser uses the function from the parent class
+print(student1.program)
+
+admin1 = AdminUser("John", "Accounting")
+admin1.introduce()
+# print(admin1.program)
+
+# StudentUser(User): this basically means StudentUser inherits from User
+# StudentUser gets access to inherited methods like introduce()
+# super().__init__(name) calls parent
+    # super is very important. because without the parent setup might not happen!
+    # double important when the parent constructor initializes important shared data.
+
+
+# The difference between abstract classes and inheritance:
+#   abstract classes define strict rules on what functions need to be implemented by the class.
+#   inheritance allows one Parent class to pass down functions and attributes to a child class.
