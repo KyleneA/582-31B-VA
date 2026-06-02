f7f3f9ea17e6fca9f31649a15a616a3709b2a762
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-06-02 10:53:50 -0400

    Adding solution for exercise 2

--- class13/exercises/kylene solution/exercise01.py
+++ class13/exercises/kylene solution/exercise01.py
@@ -18,3 +18,49 @@ animals = [Animal(), Dog(), Cat()]
 for animal in animals:
     animal.speak()
     print()
+
+# 2
+class Vehicle:
+    def __init__(self, brand):
+        self.brand = brand
+    
+    def describe(self):
+        print(f"This vehicle is from the {self.brand} brand")
+# Create a parent class: Vehicle
+# Child classes Car and Bike
+
+class Car(Vehicle):
+    def __init__(self, brand):
+        super().__init__(brand)
+    
+    def sound(self):
+        print(f"This {self.brand} car makes a vroooooom sound as it passes you")
+
+class Bike(Vehicle):
+    def __init__(self, brand):
+        super().__init__(brand)
+
+    def signal(self):
+        print(f"Ring ring, this {self.brand} bike is passing you")
+
+vehicles = [
+    Vehicle("Toyota"),
+    Car("Nissan"),
+    Bike("Norco")
+]
+
+for vehicle in vehicles:
+    vehicle.describe()
+
+    try:
+        vehicle.sound()
+    except AttributeError as e:
+        print("Error: ", e)
+
+    try:
+        vehicle.signal()
+    except AttributeError as e:
+        print("Error: ", e)
+
+    print()
+
