5cb4ee944ae3a629dea75e49c24029400aa74d69
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-06-03 10:30:08 -0400

    updating notes file & initializing exercise folder

--- class14/notes/notes.py
+++ class14/notes/notes.py
@@ -36,18 +36,64 @@
 # else: # only runs if no error raised
 #     print("Conversion Successful")
 
-# finally block
+# # finally block
+# try:
+#     x = int(input("Enter Numerator: "))
+#     y = int(input("Enter Denominator: "))
+#     division = x / y
+# except ZeroDivisionError as error:
+#     print("Error: ", error)
+# except ValueError as error:
+#     print("Error: ", error)
+# else:
+#     print(division)
+#     print("Division operation was successful")
+# finally:
+#     print("Operation is over")
+#     print()
+
+#==========================================
+
+# Class design
+
+# def set_age(age):
+#     if age < 0:
+#         raise ValueError("Age cannot be negative!")
+#     return age
+
+# try:
+#     set_age(int(input("Enter your age: ")))
+#     print()
+# except ValueError as error:
+#     print("Error: ", error)
+
+
+# Creating custom exception
+class InvalidGPAError(Exception):
+    pass
+
+class StudentRecord:
+    def __init__(self, name, gpa):
+        if not name.strip():
+            raise ValueError("Name cannot be empty")
+        self.__name = name
+        self.gpa = gpa
+    
+    @property
+    def gpa(self):
+        return self.__gpa
+    
+    @gpa.setter
+    def gpa(self, value):
+        if not(0.0 <= value <= 4.0):
+            raise InvalidGPAError("GPA must be between 0.0 and 4.0")
+        self.__gpa = value
+
+sr1 = StudentRecord("test", 4)
+
 try:
-    x = int(input("Enter Numerator: "))
-    y = int(input("Enter Denominator: "))
-    division = x / y
-except ZeroDivisionError as error:
-    print("Error: ", error)
+    sr2 = StudentRecord("", 5)
+except InvalidGPAError as error:
+    print("Could not create student record, ", error)
 except ValueError as error:
     print("Error: ", error)
-else:
-    print(division)
-    print("Division operation was successful")
-finally:
-    print("Operation is over")
-    print()
