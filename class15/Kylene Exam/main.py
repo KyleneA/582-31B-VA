46376bb59ac07a91801d50628de96c5ea71442c6
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-06-09 11:35:18 -0400

    Adding base solution for MovieShow and tests in main

--- class15/Kylene Exam/main.py
+++ class15/Kylene Exam/main.py
@@ -1,4 +1,6 @@
 from users import User, Customer, Staff
+from movie import MovieShow
+from theater import ShowStatus
 
 print("==========Customer==========")
 try:
@@ -43,5 +45,42 @@ try:
     staff1 = Staff("Bob", "bob@email.com")
     staff2 = Staff("Rob", "rob@email.com")
     print(staff1.display_info(), staff2.display_info())
+except ValueError as error:
+    print(error)
+
+print("==========MovieShow==========")
+try:
+    movie1 = MovieShow("", 30)
+    print(movie1.display_info())
+except ValueError as error:
+    print(error)
+try:
+    movie1 = MovieShow("Totoro", -30)
+    print(movie1.display_info())
+except ValueError as error:
+    print(error)
+try:
+    movie1 = MovieShow("Totoro", 30, -2)
+    print(movie1.display_info())
+except ValueError as error:
+    print(error)
+try:
+    movie1 = MovieShow("Totoro", 30, 32)
+    print(movie1.display_info())
+except ValueError as error:
+    print(error)
+try:
+    movie1 = MovieShow("Totoro", 30, 2, "open")
+    print(movie1.display_info())
+except ValueError as error:
+    print(error)
+try:
+    movie1 = MovieShow("Totoro", 30)
+    print(movie1.display_info())
+except ValueError as error:
+    print(error)
+try:
+    movie1 = MovieShow("Totoro", 30, 2, ShowStatus.OPEN)
+    print(movie1.display_info())
 except ValueError as error:
     print(error)
---
+++ class15/Kylene Exam/movie.py
@@ -0,0 +1,72 @@
+from theater import TheaterRoom, ScreaningTime, ShowStatus
+from helperMethods import HelperMethods
+
+class MovieShow:
+    MAX_TICKETS_PER_BOOKING = 10
+
+    def __init__(self, title, capacity, booked_seats = 0, status = ShowStatus.OPEN):
+        if not HelperMethods.is_valid_input(title, 0):
+            raise ValueError("Movie title must contain at least 1 letter")
+        else:
+            self.__title = title
+            self.capacity = capacity
+            self.booked_seats = booked_seats
+            self.status = status
+    
+    @property
+    def capacity(self):
+        return self.__capacity
+    
+    @capacity.setter
+    def capacity(self, value):
+        if value <= 0:
+            raise ValueError("Capacity must be a positive number greater than 0")
+        else:
+            self.__capacity = value
+    
+    @property
+    def booked_seats(self):
+        return self.__booked_seats
+    
+    @booked_seats.setter
+    def booked_seats(self, value):
+        if value < 0:
+            raise ValueError("Number of booked seats must be a positive number")
+        elif value > self.__capacity:
+            raise ValueError("Number of booked seats cannot exceed capacity")
+        else:
+            self.__booked_seats = value
+    
+    @property
+    def status(self):
+        return self.__status
+    
+    @status.setter
+    def status(self, value):
+        if not isinstance(value, ShowStatus):
+            raise ValueError("Status must be a member of ShowStatus")
+        else:
+            self.__status = value
+
+    def remaining_seats(self):
+        return self.capacity - self.booked_seats
+
+    def display_info(self):
+        return f"Title: {self.__title} | Capacity: {self.capacity} | Status: {self.status.value} | Booked seats: {self.booked_seats}, Remaining seats: {self.remaining_seats()}"
+
+    def book_tickets(self, customer, quantity):
+        if quantity < 0:
+            raise ValueError("Quantity must be greater than 0")
+        elif quantity > MovieShow.MAX_TICKETS_PER_BOOKING:
+            raise ValueError(f"You cannot book more than {MovieShow.MAX_TICKETS_PER_BOOKING} per session")
+        elif self.status == ShowStatus.CANCELLED:
+            raise ValueError("Booking is closed because the showing has been cancelled")
+        elif self.status == ShowStatus.SOLD_OUT:
+            raise ValueError("Booking is closed because the showing is sold out")
+        elif self.booked_seats + quantity > self.capacity:
+            raise ValueError(f"You cannot book {quantity} tickets. There are only {self.remaining_seats()} seats available.")
+        else:
+            self.booked_seats += quantity
+            if self.booked_seats == self.capacity:
+                self.status = ShowStatus.SOLD_OUT
+    
