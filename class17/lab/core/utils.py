cbbc4f751e5976fbe87e22832ab9b777c57f43a9
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-06-16 10:49:03 -0400

    Creating a helper class with helper method to more easily display class information and using it in main

---
+++ class17/lab/core/utils.py
@@ -0,0 +1,5 @@
+class Helper:
+    def displayHelper(array):
+        for item in array:
+            item.display_info()
+            print("==========")
--- class17/lab/main.py
+++ class17/lab/main.py
@@ -1,5 +1,6 @@
 from core.constants import MAX_TICKETS_PER_BOOKING
 from core.enums import ShowStatus
+from core.utils import Helper
 from models.customer import Customer
 from models.staff import Staff
 from models.movie_show import MovieShow
@@ -8,10 +9,14 @@ def main():
     customer = Customer("Ava")
     staff = Staff("Sam")
     show = MovieShow("Inception", 20, ShowStatus.OPEN)
+    to_display = [customer, staff, show]
 
-    customer.display_info()
-    staff.display_info()
-    show.display_info()
+    Helper.displayHelper(to_display)
+
+    # customer.display_info()
+    # staff.display_info()
+    # show.display_info()
+    
     print("Max tickets per booking:", MAX_TICKETS_PER_BOOKING)
 
 if __name__ == "__main__":
